5bb6df0f011370bcfd3c9a02f17b3c8b86759480
Louis Claremont <lpclaremont@gmail.com>
2023-09-08 09:32:50 +0100

    #deposit method created and tested

--- lib/vending_machine.py
+++ lib/vending_machine.py
@@ -1,7 +1,7 @@
 class VendingMachine:
     def __init__(self):
         self.coin_bank = {}
-        self.deposited_funds = {}
+        self.deposited_funds = 0
 
     def load_coins(self, two_pound, pound, fifty, twenty, ten, five, two, one):
         quantities = [two_pound, pound, fifty, twenty, ten, five, two, one]
@@ -15,8 +15,17 @@ class VendingMachine:
         self.coin_bank[0.05] = five
         self.coin_bank[0.02] = two
         self.coin_bank[0.01] = one
+    
+    def deposit(self, coin_value):
+        self.__check_deposit_value(coin_value)
+        self.deposited_funds += coin_value
 
     def __check_quantities(self, quantities):
         for q in quantities:
             if q < 0 or type(q) != int:
                 raise TypeError('Coin quantities must be non negative integers')
+    
+    def __check_deposit_value(self, coin_value):
+        valid_values = [2, 1, 0.5, 0.2, 0.10, 0.05, 0.02, 0.01]
+        if coin_value not in valid_values:
+            raise ValueError('Deposits must be a valid UK coin denomination')
--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -8,11 +8,11 @@ an empty coin bank and deposited funds
 def test_vending_machine_initialised():
     machine = VendingMachine()
     assert machine.coin_bank == {}
-    assert machine.deposited_funds == {}
+    assert machine.deposited_funds == 0
 
 """
-Coin bank is updated with correct
-quantitites when #load_coins method is called
+#load_coins updates the coin_bank 
+with correct quantitites when called
 """
 def test_loading_coins():
     machine = VendingMachine()
@@ -30,3 +30,23 @@ def test_invalid_coin_quantity():
     with pytest.raises(TypeError) as err_info:
         machine.load_coins(-2, 1, 1, 1, 1, 1, 1, 1)
     assert str(err_info.value) == 'Coin quantities must be non negative integers'
+
+"""
+#deposit_coin updates deposited_funds
+with correct coin when called
+"""
+def test_deposit_coin():
+    machine = VendingMachine()
+    machine.deposit(0.5)
+    machine.deposit(0.2)
+    assert machine.deposited_funds == 0.7
+
+"""
+#deposit_coin throws an error when
+value is not a valid coin amount
+"""
+def test_invalid_deposit():
+    machine = VendingMachine()
+    with pytest.raises(ValueError) as err_info:
+        machine.deposit(0.32)
+    assert str(err_info.value) == 'Deposits must be a valid UK coin denomination'
