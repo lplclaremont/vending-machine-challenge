eddfc76b3f4d4b336ab668232d387d46a2efc048
Louis Claremont <lpclaremont@gmail.com>
2023-09-08 08:51:31 +0100

    #load_coins method created and tested

--- lib/vending_machine.py
+++ lib/vending_machine.py
@@ -2,3 +2,21 @@ class VendingMachine:
     def __init__(self):
         self.coin_bank = {}
         self.deposited_funds = {}
+
+    def load_coins(self, two_pound, pound, fifty, twenty, ten, five, two, one):
+        quantities = [two_pound, pound, fifty, twenty, ten, five, two, one]
+        self.__check_quantities(quantities)
+
+        self.coin_bank[2] = two_pound
+        self.coin_bank[1] = pound
+        self.coin_bank[0.5] = fifty
+        self.coin_bank[0.2] = twenty
+        self.coin_bank[0.1] = ten
+        self.coin_bank[0.05] = five
+        self.coin_bank[0.02] = two
+        self.coin_bank[0.01] = one
+
+    def __check_quantities(self, quantities):
+        for q in quantities:
+            if q < 0 or type(q) != int:
+                raise TypeError('Coin quantities must be non negative integers')
--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -1,3 +1,4 @@
+import pytest
 from lib.vending_machine import VendingMachine
 
 """
@@ -8,3 +9,24 @@ def test_vending_machine_initialised():
     machine = VendingMachine()
     assert machine.coin_bank == {}
     assert machine.deposited_funds == {}
+
+"""
+Coin bank is updated with correct
+quantitites when #load_coins method is called
+"""
+def test_loading_coins():
+    machine = VendingMachine()
+    machine.load_coins(7,6,5,4,3,2,1,0)
+    assert machine.coin_bank == {
+        2: 7, 1: 6, 0.5: 5, 0.2: 4, 0.1: 3, 0.05: 2, 0.02: 1, 0.01: 0
+    }
+
+"""
+#load_coins throws an error when the input
+are not valid coin quantities
+"""
+def test_invalid_coin_quantity():
+    machine = VendingMachine()
+    with pytest.raises(TypeError) as err_info:
+        machine.load_coins(-2, 1, 1, 1, 1, 1, 1, 1)
+    assert str(err_info.value) == 'Coin quantities must be non negative integers'
